f763becfad98805d4a6b208b91e6cf6ce567febb
Hiroki Ariji <ariji.hiroki@O-05166-MAC.local>
2015-09-28 00:55:15 +0900

    update uri string

--- Classes/EstateScene.cpp
+++ Classes/EstateScene.cpp
@@ -143,21 +143,27 @@ void EstateController::initGame(int type)
     switch (type) {
         case 1:
             wording = "住宅相談";
+            user_status = BUILD_SHOHIN_CONSULT;
             break;
         case 2:
             wording = "リフォームしたいけど\n何から手をつけたらいいかわからない・・・。\nリフォームに関するおすすめ情報です";
+            user_status = BASIC_REFORM_FLOW;
             break;
         case 3:
             wording = "賃貸住宅D-Room";
+            user_status = RENT_HOUSE;
             break;
         case 4:
             wording = "住宅相談（機能面）";
+            user_status = REGIST_KENCHIKU_CONSULT;
             break;
         case 5:
             wording = "住宅リフォーム（機能面）";
+            user_status = KEEP_LIVING_BUILDING;
             break;
         case 6:
             wording = "住宅リフォーム（金額面）";
+            user_status = KEEP_PROCE100_BUILDING;
             break;
             
         default:
@@ -208,123 +214,158 @@ void EstateController::onTouchEnded(Touch *touch, Event *unused_event)
 {
     const char*uri;
 
-    if (user_status & REGIST_KENCHIKU_CONSULT){
+    if (user_status == REGIST_KENCHIKU_CONSULT){
         // 建築物の商品相談
         uri = "https://www.daiwahouse.co.jp/jutaku/soudan/shohin/index.html";
-    }else if(user_status & KEEP_CHILD_BUILDING){
+    }else if(user_status == KEEP_CHILD_BUILDING){
         // 子育てしやすい住宅
         uri = "https://www.daiwahouse.co.jp/jutaku/soudan/qa/regist.asp?type=5";
-    }else if(user_status & KEEP_2SETAI_BUILDING){
+    }else if(user_status == KEEP_2SETAI_BUILDING){
         // 二世帯住宅
         uri = "https://www.daiwahouse.co.jp/jutaku/soudan/qa/regist.asp?type=7";
-    }else if(user_status & KEEP_PET_BUILDING){
+    }else if(user_status == KEEP_PET_BUILDING){
         // ペットと一緒の暮らし
         uri = "https://www.daiwahouse.co.jp/jutaku/soudan/qa/regist.asp?type=6";
-    }else if(user_status & KEEP_HIRAYA_BUILDING){
+    }else if(user_status == KEEP_HIRAYA_BUILDING){
         // 平屋の暮らし
         uri = "https://www.daiwahouse.co.jp/jutaku/soudan/qa/regist.asp?type=14";
-    }else if(user_status & KEEP_CO_TINTAI_BUILDING){
+    }else if(user_status == KEEP_CO_TINTAI_BUILDING){
         // 賃貸併用の暮らし
         uri = "https://www.daiwahouse.co.jp/jutaku/soudan/qa/regist.asp?type=8";
-    }else if(user_status & KEEP_HOISPITAL_BUILDING){
+    }else if(user_status == KEEP_HOISPITAL_BUILDING){
         //　医院併用の暮らし
         uri = "https://www.daiwahouse.co.jp/jutaku/soudan/qa/regist.asp?type=37";
-    }else if(user_status & KEEP_1ROOM_RENT_BUILDING){
+    }else if(user_status == KEEP_1ROOM_RENT_BUILDING){
         //　１部屋賃貸（習い事教室）の暮らし
         uri = "https://www.daiwahouse.co.jp/eventHP/regist.asp?event_id=110790";
-    }else if(user_status & KEEP_RENT_MYHOME_BUILDING){
+    }else if(user_status == KEEP_RENT_MYHOME_BUILDING){
         //　マイホーム借り上げ制度
         uri = "https://www.daiwahouse.co.jp/jutaku/soudan/qa/regist.asp?type=38";
-    }else if(user_status & KEEP_SHUNOU_BUILDING){
+    }else if(user_status == KEEP_SHUNOU_BUILDING){
         // 収納上手な暮らし
         uri = "https://www.daiwahouse.co.jp/jutaku/soudan/qa/regist.asp?type=11";
-    }else if(user_status & KEEP_LOVELY_KITCHIN_BUILDING){
+    }else if(user_status == KEEP_LOVELY_KITCHIN_BUILDING){
         // 愛着キッチンのある暮らし
         uri = "https://www.daiwahouse.co.jp/jutaku/soudan/qa/regist.asp?type=12";
-    }else if(user_status & KEEP_SMART_BUILDING){
+    }else if(user_status == KEEP_SMART_BUILDING){
         // ダイワハウスのスマートハウス
         uri = "https://www.daiwahouse.co.jp/jutaku/soudan/qa/regist.asp?type=4";
-    }else if(user_status & KEEP_TAIYOU_BUILDING){
+    }else if(user_status == KEEP_TAIYOU_BUILDING){
         // 太陽発電のある暮らし
         uri = "https://www.daiwahouse.co.jp/jutaku/soudan/qa/regist.asp?type=1";
-    }else if(user_status & KEEP_MOVIE_SOUND_BUILDING){
+    }else if(user_status == KEEP_MOVIE_SOUND_BUILDING){
         // 映画、音楽のある暮らし
         uri = "https://www.daiwahouse.co.jp/jutaku/soudan/qa/regist.asp?type=13";
-    }else if(user_status & KEEP_WOMAN_BUILDING){
+    }else if(user_status == KEEP_WOMAN_BUILDING){
         //　女性のための空間提案
         uri = "https://www.daiwahouse.co.jp/jutaku/soudan/qa/regist.asp?type=16";
-    }else if(user_status & KEEP_DESIGNERS_BUILDING){
+    }else if(user_status == KEEP_DESIGNERS_BUILDING){
         //　デザイナーズ住宅
         uri = "https://www.daiwahouse.co.jp/jutaku/soudan/qa/regist.asp?type=24";
-    }else if(user_status & KEEP_CESON_BUILDING){
+    }else if(user_status == KEEP_CESON_BUILDING){
         //　キャトル、セゾンが似合う住宅
         uri = "https://www.daiwahouse.co.jp/eventHP/regist.asp?event_id=102344";
-    }else if(user_status & KEEP_GREEN_BUILDING){
+    }else if(user_status == KEEP_GREEN_BUILDING){
         //　緑に囲まれた暮らし
         uri = "https://www.daiwahouse.co.jp/jutaku/soudan/qa/regist.asp?type=15";
     }
-/*
-    // 住まい体験
-    uri = "http://www.daiwahouse.co.jp/jutaku/building/experience/index.html";
+
+    if(user_status == EXPERIENCE_BUILDING){
+        // 住まい体験
+        uri = "http://www.daiwahouse.co.jp/jutaku/building/experience/index.html";
+    }
 
     
     // リフォーム：基礎知識
-    // 住まいの隠れた問題点も発見。
-    uri = "http://www.daiwahouse-reform.co.jp/guide/inspection/index.html";
-    // リフォームの流れがわかる手順・段取
-    uri = "http://www.daiwahouse-reform.co.jp/guide/flow/index.html";
-    // 住まいのメンテナンススケジュール
-    uri = "http://www.daiwahouse-reform.co.jp/guide/maintenance/index.html";
-    // リフォームの「？」がわかるQ&A
-    uri = "http://www.daiwahouse-reform.co.jp/guide/qa/index.html";
-
-    // 賃貸相談：：
-    uri = "https://www.daiwahouse.co.jp/chintai/";
-
-    // 注文住宅：：土地探し
-    uri = "http://www.daiwahouse.co.jp/jutaku/soudan/tochi/index.asp";
-    // 間取り相談
-    uri = "http://www.daiwahouse.co.jp/jutaku/soudan/madori/index.asp";
-    // 建築相談
-    uri = "https://www.daiwahouse.co.jp/smp/business/kenchiku/script/regist_kenchiku.asp?ken_toi_kbn=3";
-    // 帰国前相談
-    uri = "http://www.daiwahouse.co.jp/jutaku/soudan/kaigai/index.asp";
-    //　相続税軽減に関する相談
-    uri = "https://www.daiwahouse.co.jp/jutaku/soudan/qa/regist.asp?type=2";
-    //　住まいの各種ほぞ金などの相談
-    uri = "https://www.daiwahouse.co.jp/jutaku/soudan/qa/regist.asp?type=3";
-    //　住まいずくりに対する全般的な相談
-    uri = "https://www.daiwahouse.co.jp/jutaku/soudan/qa/regist.asp?type=32";
+    if(user_status == BASIC_REFORM_INSPECT){
+        // 住まいの隠れた問題点も発見。
+        uri = "http://www.daiwahouse-reform.co.jp/guide/inspection/index.html";
+    }else if(user_status == BASIC_REFORM_FLOW){
+        // リフォームの流れがわかる手順・段取
+        uri = "http://www.daiwahouse-reform.co.jp/guide/flow/index.html";
+    }else if(user_status == BASIC_REFORM_MAINTENANCE){
+        // 住まいのメンテナンススケジュール
+        uri = "http://www.daiwahouse-reform.co.jp/guide/maintenance/index.html";
+    }else if(user_status == BASIC_REFORM_QA){
+        // リフォームの「？」がわかるQ&A
+        uri = "http://www.daiwahouse-reform.co.jp/guide/qa/index.html";
+    }
+        
+    if(user_status == RENT_HOUSE){
+        // 賃貸相談：：
+        uri = "https://www.daiwahouse.co.jp/chintai/";
+    }
+    
+    if(user_status == SEARCH_ESTATE_CONSULT){
+        // 注文住宅：：土地探し
+        uri = "http://www.daiwahouse.co.jp/jutaku/soudan/tochi/index.asp";
+    }else if(user_status == MADORI_CONSULT){
+        // 間取り相談
+        uri = "http://www.daiwahouse.co.jp/jutaku/soudan/madori/index.asp";
+    }else if(user_status == BUILD_SHOHIN_CONSULT){
+        // 建築相談
+        uri = "https://www.daiwahouse.co.jp/smp/business/kenchiku/script/regist_kenchiku.asp?ken_toi_kbn=3";
+    }else if(user_status == KAIGAI_MOVE_CONSULT){
+        // 帰国前相談
+        uri = "http://www.daiwahouse.co.jp/jutaku/soudan/kaigai/index.asp";
+    }else if(user_status == KEEP_TAX_BUILDING){
+        //　相続税軽減に関する相談
+        uri = "https://www.daiwahouse.co.jp/jutaku/soudan/qa/regist.asp?type=2";
+    }else if(user_status == KEEP_OTHERMONEY_BUILDING){
+        //　住まいの各種ほぞ金などの相談
+        uri = "https://www.daiwahouse.co.jp/jutaku/soudan/qa/regist.asp?type=3";
+    }else if(user_status == KEEP_OVERALL_BUILDING){
+        //　住まいずくりに対する全般的な相談
+        uri = "https://www.daiwahouse.co.jp/jutaku/soudan/qa/regist.asp?type=32";
+    }
 
     // リフォーム
-    // 玄関
-    uri = "http://www.daiwahouse-reform.co.jp/search/part/entrance_pricemap.html";
-    // 外観
-    uri = "http://www.daiwahouse-reform.co.jp/search/part/outside_pricemap.html";
-    // 屋根
-    uri = "http://www.daiwahouse-reform.co.jp/search/part/outside_pricemap.html";
-    // 和室
-    uri = "http://www.daiwahouse-reform.co.jp/search/part/japanese_pricemap.html";
-    // 防音、ホームシアター
-    uri = "http://www.daiwahouse-reform.co.jp/search/theme/hometheater_pricemap.html";
-    // リビング
-    uri = "http://www.daiwahouse-reform.co.jp/search/part/living_pricemap.html";
-    // トイレ
-    uri = "http://www.daiwahouse-reform.co.jp/search/part/lavatory_pricemap.html";
-    // キッチン
-    uri = "http://www.daiwahouse-reform.co.jp/search/part/kitchen_pricemap.html";
-    //　洗面室
-    uri = "http://www.daiwahouse-reform.co.jp/search/part/lavatory_pricemap.html";
-    // 風呂場
-    uri = "http://www.daiwahouse-reform.co.jp/search/part/bath_pricemap.html";
+    if(user_status == KEEP_ENTRANCE_BUILDING){
+        // 玄関
+        uri = "http://www.daiwahouse-reform.co.jp/search/part/entrance_pricemap.html";
+    }else if(user_status == KEEP_OUTSIDE_BUILDING){
+        // 外観
+        uri = "http://www.daiwahouse-reform.co.jp/search/part/outside_pricemap.html";
+    }else if(user_status == KEEP_UPPER_OUTSIDE_BUILDING){
+        // 屋根
+        uri = "http://www.daiwahouse-reform.co.jp/search/part/outside_pricemap.html";
+    }else if(user_status == KEEP_JAPANESE_BUILDING){
+        // 和室
+        uri = "http://www.daiwahouse-reform.co.jp/search/part/japanese_pricemap.html";
+    }else if(user_status == KEEP_HOMETHEATER_BUILDING){
+        // 防音、ホームシアター
+        uri = "http://www.daiwahouse-reform.co.jp/search/theme/hometheater_pricemap.html";
+    }else if(user_status == KEEP_LIVING_BUILDING){
+        // リビング
+        uri = "http://www.daiwahouse-reform.co.jp/search/part/living_pricemap.html";
+    }else if(user_status == KEEP_TOILET_BUILDING){
+        // トイレ
+        uri = "http://www.daiwahouse-reform.co.jp/search/part/lavatory_pricemap.html";
+    }else if(user_status == KEEP_KITCHEN_BUILDING){
+        // キッチン
+        uri = "http://www.daiwahouse-reform.co.jp/search/part/kitchen_pricemap.html";
+    }else if(user_status == KEEP_LAVATORY_BUILDING){
+        //　洗面室
+        uri = "http://www.daiwahouse-reform.co.jp/search/part/lavatory_pricemap.html";
+    }else if(user_status == KEEP_BATH_BUILDING){
+        // 風呂場
+        uri = "http://www.daiwahouse-reform.co.jp/search/part/bath_pricemap.html";
+    }
+
     // リフォーム予算
-    // 100万円未満
-    uri = "http://www.daiwahouse-reform.co.jp/search/price/under100_pricemap.html";
-    // 200万円未満
-    uri = "http://www.daiwahouse-reform.co.jp/search/price/100-200_pricemap.html";
-    // 200万円から
-    uri = "http://www.daiwahouse-reform.co.jp/search/price/200-300_pricemap.html";
-*/
+    if(user_status == KEEP_PROCE100_BUILDING){
+        // 100万円未満
+        uri = "http://www.daiwahouse-reform.co.jp/search/price/under100_pricemap.html";
+    }else if(user_status == KEEP_PRICE200_BUILDING){
+        // 200万円未満
+        uri = "http://www.daiwahouse-reform.co.jp/search/price/100-200_pricemap.html";
+    }else  if(user_status == KEEP_PRICE_OVER200_BUILDING){
+        // 200万円から
+        uri = "http://www.daiwahouse-reform.co.jp/search/price/200-300_pricemap.html";
+    }
+
+    
+
     startWebView(uri);
     return;
 }
